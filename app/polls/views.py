f5f898046c28f7e8d6d7f7781734c251a34b6a3f
Seongwon Han <seongwonhan88@gmail.com>
2018-10-02 16:00:45 +0900

    index, detail, results, vite view url 연결  index view는 가장 최근에 발행된 question 까지 보여줌

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
