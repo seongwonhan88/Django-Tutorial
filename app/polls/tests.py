6213048daa17f7799704be44541522c3a7f75e1d
Seongwon Han <seongwonhan88@gmail.com>
2018-10-04 15:59:56 +0900

    QuestionIndexView test append

--- app/polls/tests.py
+++ app/polls/tests.py
@@ -58,6 +58,6 @@ class QuestionIndexViewTests(TestCase):
         create_question(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
-            response.context['latest_qustion_list'],
+            response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
--- app/polls/views/cbv.py
+++ app/polls/views/cbv.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic, View
 
 from ..models import Question, Choice
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return super().get_queryset().order_by('-pub_date')[:5]
+        return super().get_queryset().filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DeleteView):
     model = Question
